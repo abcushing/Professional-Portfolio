90a1d63d3e559e741c0da16f5e1b76b8d93c8cb1
abcushing <abcushing789@gmail.com>
2023-01-30 13:11:31 -0700

    5th-commit
    5th commit, got rid of the errors but lost my navbar

---
+++ src/components/Page/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Portfolio from "../Portfolio";
+import Resume from "../Resume/index.js";
+import About from "../About";
+import Contact from "../Contact";
+import PageContent from "../PageContent";
+
+function Page({ currentPage }) {
+  const renderPage = () => {
+    switch (currentPage.name) {
+      case "about":
+        return <About />;
+      case "contact":
+        return <Contact />;
+      case "portfolio":
+        return <Portfolio />;
+      case "resume":
+        return <Resume />;
+    }
+  };
+  // const handlePageChange = (page) => setCurrentPage(page);
+  return (
+    <PageContent>
+      <h2>{renderPage()}</h2>
+    </PageContent>
+  );
+}
+
+export default Page;
--- src/components/PortfolioContainer/index.js
+++ src/components/PortfolioContainer/index.js
@@ -1,39 +1,37 @@
 import { useState } from "react";
 import React from "react";
-import About from "../About";
-import Contact from "../Contact";
 import Navbar from "../Navbar";
-import Resume from "../Resume/index.js";
-import Portfolio from "../Portfolio";
+import Page from "../Page";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState("About");
-
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === "About") {
-      return <About />;
-    }
-    if (currentPage === "Contact") {
-      return <Contact />;
-    }
-    if (currentPage === "Portfolio") {
-      return <Portfolio />;
-    }
-    if (currentPage === "Resume") {
-      return <Resume />;
-    }
-    return <Contact />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
+  const [pages] = useState([
+    {
+      name: "about",
+    },
+    {
+      name: "contact",
+    },
+    {
+      name: "portfolio",
+    },
+    {
+      name: "resume",
+    },
+  ]);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
-      {renderPage()}
+      <Navbar
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        // handlePageChange={handlePageChange}
+      ></Navbar>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      {/* <About /> */}
     </div>
   );
 }
