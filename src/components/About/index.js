610ac73117f48de5036bc36880c8a524d67341e5
abcushing <abcushing789@gmail.com>
2023-03-24 11:49:33 -0400

    added links
    added links from other project, although I keep getting errors still...

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Contact from "../Contact/index";
+import Navbar from "../Navbar/index";
+import Portfolio from "../Portfolio/index";
+import Page from "../Page";
+
+const NavBar = ({ Contact, Portfolio }) => {
+  const search = () => {
+    setCurrent("Page");
+  };
+  return (
+    <div>
+      <Link onClick={() => setCurrent("About")}>
+        About &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </Link>
+      <Link onClick={() => setCurrent("Portfolio")}>
+        Portfolio &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </Link>
+      <Link onClick={() => setCurrent("Contact")}>
+        Contact &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </Link>
+      <br></br>
+      <br></br>
+    </div>
+  );
+};
 
 const About = () => {
   return (
