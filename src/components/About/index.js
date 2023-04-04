9a4af72959c9131fd6356c55639db619f3e003c0
abcushing <abcushing789@gmail.com>
2023-04-04 09:23:11 -0400

    added links to projects in notes
    added links to the demo projects on my portfolio. the links are currently just comments with repo links

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,7 +21,8 @@ const About = () => {
             </img> */}
             <p>
               blurb hi im a human who recently finished a coding bootcamp
-              through denver university's online program
+              through denver university's online program. Definatly not two kids
+              in a chatbot GPT suit
             </p>
             {/* link to pdf bootcamp badges */}
           </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,12 +8,30 @@ const Portfolio = () => {
         <div class="col-6">
           <h2>Examples of my Work</h2>
           <p>(in chronological order)</p>
-          <h4>Project 1(2)</h4>
-          <h4>Project 2(6)</h4>
-          <h4>Project 3(9)</h4>
-          <h4>Project 4(12)</h4>
-          <h4>Project 5(13)</h4>
-          <h4>Project 6(18)</h4>
+          <h4>
+            Project 1(2)
+            {/* https://github.com/abcushing/challange-002 */}
+          </h4>
+          <h4>
+            Project 2(6)
+            {/* https://github.com/abcushing/Challange006-WeatherApp */}
+          </h4>
+          <h4>
+            Project 3(9)
+            {/* https://github.com/abcushing/Challange09-ReadMe */}
+          </h4>
+          <h4>
+            Project 4(12)
+            {/* https://github.com/abcushing/Employee-Tracker */}
+          </h4>
+          <h4>
+            Project 5(13)
+            {/* https://github.com/abcushing/E-commerce-Back-End */}
+          </h4>
+          <h4>
+            Project 6(18)
+            {/* https://github.com/abcushing/socialnetworkapi-nosql */}
+          </h4>
         </div>
         <div class="col"></div>
       </div>
