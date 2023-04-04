49a601115d52d40e5fcaaf5fca1d3269348f66bf
abcushing <abcushing789@gmail.com>
2023-04-04 12:30:31 -0400

    finished portfolio info, added links
    finished portfolio information for now, added links to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,11 +8,23 @@ const Contact = () => {
         <div class="col-6">
           {/* <p> idk if I want to put my email here or not </p> */}
           <p>
-            linkedin
+            <a
+              href="https://www.linkedin.com/in/alex-cushing-1353b6149/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
             {/* https://www.linkedin.com/in/alex-cushing-1353b6149/ */}
           </p>
           <p>
-            github
+            <a
+              href="https://github.com/abcushing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
             {/* https://github.com/abcushing */}
           </p>
         </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -36,27 +36,39 @@ const Portfolio = () => {
           <br></br>
           <h4>Project: Employee Tracker</h4>
           <p>
-            This was the twetlth project I worked on. it is a command line
+            This was the twetlth project I worked on. It is a command line
             application that can be used as an employee database. This control
             managment system was built using Node.js, Inquirer, and MySQL (I
             have a link to a demo video below){" "}
           </p>
           {/* https://github.com/abcushing/Employee-Tracker project 12*/}
           <br></br>
-          <h4>Project: E-commerce-Back</h4>
+          <h4>Project: E-Commerce-Back</h4>
+          <p>
+            This was the thirteenth project I worked on. It is the backend of an
+            e-commerce site. I had to use an Express.js API and configure it to
+            work with a MySQL database. Sequelize can be used with to interact
+            with the database.(I have a link to a demo video below)
+          </p>
           {/* https://github.com/abcushing/E-commerce-Back-End project 13*/}
           <br></br>
           <h4>Project: Social Network API</h4>
-          {/* https://github.com/abcushing/socialnetworkapi-nosql project 14 -turned off heroku so I would not have to pay for this app*/}
+          <p>
+            This was the eighteenth project I worked on (and one of the most
+            complicated so far!) It is an API for a social network app. I used
+            Express.js for routing and MongoDB Database with Mongoose ODM (I
+            have a link to a demo video below)
+          </p>
+          {/* https://github.com/abcushing/socialnetworkapi-nosql project 18 -turned off heroku so I would not have to pay for this app*/}
           <br></br>
           <h4>Group Project: Mordle</h4>
-          {/* https://github.com/abcushing/copy_mordle group project 1*/}
           <p>
             out of the 3 group projects, this was the most original project I
             worked on. We decided to make a wordle app (word guessing game) but
             give it a movie theme. This was a collaboration between myself and 2
             other classmates
           </p>
+          {/* https://github.com/abcushing/copy_mordle group project 1*/}
           <br></br>
         </div>
         <div class="col"></div>
